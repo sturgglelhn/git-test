2a50919a867ff0cbb4c435baf8acc4c722fc3ad1
Adolf <Adolf@atguigu.com>
2022-09-19 16:48:59 +0800

    IDEA third commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -5,5 +5,7 @@ public class GitTest {
         System.out.println("Hello Git!");
         System.out.println("Hello Git!");
         System.out.println("Hello Git!");
+        System.out.println("Hello Git4!");
+        System.out.println("Hello Git6!");
     }
 }
